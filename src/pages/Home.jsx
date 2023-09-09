13f51288721fadc675669e1f4881837fd5c77e8b
Sparsh Kadian <sparshkadian323@gmail.com>
2023-09-09 22:17:03 +0530

    adding Bookmark to Profile card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import { getAuth } from 'firebase/auth';
 import { Link } from 'react-router-dom';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from 'react';
+import Lottie from 'lottie-react';
+import bookmarkAnimation from './../animations/bookmarkAnimation.json';
 
 const Home = () => {
   const auth = getAuth();
   const [dogData, setDogData] = useState([]);
+  const [selectedProfiles, setSelectedProfiles] = useState([]);
 
   useEffect(() => {
     getData();
@@ -17,6 +21,27 @@ const Home = () => {
       data: { dogs },
     } = await response.json();
     setDogData(dogs);
+  };
+
+  const disableBookmark = (dogId) => {
+    setSelectedProfiles(
+      selectedProfiles.filter((id) => {
+        return id !== dogId;
+      })
+    );
+  };
+
+  const enableBookmark = (id) => {
+    setSelectedProfiles((prevState) => {
+      return [...prevState, id];
+    });
+  };
+
+  const checkSelected = (id) => {
+    for (let i = 0; i < selectedProfiles.length; i++) {
+      if (id === selectedProfiles[i]) return true;
+      else return false;
+    }
   };
 
   return (
@@ -47,11 +72,31 @@ const Home = () => {
       <div className='w-4/5 m-auto grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-10'>
         {dogData.map((dog, i) => {
           return (
-            <div
-              key={i}
-              className='border-2 border-zinc-200 rounded-md flex flex-col'
-            >
-              <div className='dogImg-container h-[250px]'>
+            <div key={i} className='relative rounded-md flex flex-col'>
+              {checkSelected(dog._id) && (
+                <div className='cursor-pointer absolute w-[80px] -top-[24px] right-[-12px]'>
+                  {' '}
+                  <Lottie
+                    onClick={() => {
+                      disableBookmark(dog._id);
+                    }}
+                    animationData={bookmarkAnimation}
+                  />
+                </div>
+              )}
+
+              {!checkSelected(dog._id) && (
+                <div className='cursor-pointer overflow-hidden absolute -top-1 right-1'>
+                  <img
+                    onClick={() => {
+                      enableBookmark(dog._id);
+                    }}
+                    src='../bookmark-disabled.png'
+                    className='text-red-300'
+                  />
+                </div>
+              )}
+              <div className='h-[250px]'>
                 <img
                   src={dog.photoURL}
                   className='w-full h-full rounded-t-md'
@@ -59,7 +104,7 @@ const Home = () => {
                 />
               </div>
 
-              <div className='flex flex-col gap-4 p-2'>
+              <div className='border-2 border-zinc-200 border-t-0 flex flex-col gap-4 p-2'>
                 <div>
                   <p className='font-semibold'>
                     Name: <span className='font-normal'>{dog.name}</span>
@@ -87,6 +132,14 @@ const Home = () => {
                     Contact: <span className='font-normal'>{dog.contact}</span>
                   </p>
                 </div>
+
+                <button
+                  onClick={() => console.log(dog.name)}
+                  type='btn'
+                  className='btn w-1/2 m-auto my-3'
+                >
+                  Save
+                </button>
               </div>
             </div>
           );
